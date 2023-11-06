db035b0825e29e91338904d8b5946be1c7408b12
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-11-06 15:25:33 +0000

    Add user profiles

--- app/components/Lobby.tsx
+++ app/components/Lobby.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import InviteButton from './Button/InviteButton';
+import UserProfile from './UserProfile';
 
 const Lobby = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      <div>Lobby</div>;
+      <h1>Lobby</h1>
+      <UserProfile />
+      <InviteButton />
     </main>
   );
 };
--- app/components/UserProfile.tsx
+++ app/components/UserProfile.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import avatar1 from '../../public/images/avatars/avatar1.jpg';
+import avatar2 from '../../public/images/avatars/avatar2.jpg';
+import Image from 'next/image';
 
 const UserProfile = () => {
-  return <div>UserProfile</div>;
+  return (
+    <>
+      <div>
+        <Image key={1} src={avatar1} width={20} height={20} alt="userAvatar" />
+        <p>User1</p>
+
+        <Image key={2} src={avatar2} width={20} height={20} alt="userAvatar" />
+        <p>User2</p>
+      </div>
+    </>
+  );
 };
 
 export default UserProfile;
