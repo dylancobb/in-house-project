5abe6c33ccc7b927f20824c2d27f49bc73a4ce19
Dylan <122552492+dylancobb@users.noreply.github.com>
2023-11-06 14:29:45 +0000

    make avatar selection loop

--- app/components/AvatarSlider/AvatarSlider.tsx
+++ app/components/AvatarSlider/AvatarSlider.tsx
@@ -16,73 +16,61 @@ export default function App({
   setCurrentSlide
 }: AppProps) {
   const [loaded, setLoaded] = useState(false);
-  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>(
-    {
-      initial: 0,
-      slideChanged(s) {
-        setCurrentSlide(s.track.details.rel);
-      },
-      created() {
-        setLoaded(true);
-      },
-    }
-    // [AdaptiveHeight]
-  );
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    loop: true,
+    initial: 0,
+    slideChanged(s) {
+      setCurrentSlide(s.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
+  });
 
   return (
-    <>
-      <div className="navigation-wrapper">
-        <div ref={sliderRef} className="keen-slider">
-          {Object.values(avatars).map((avatar, index) => (
-            <div key={index} className="keen-slider__slide number-slide">
-              <Image
-                className="rounded-full"
-                src={avatar}
-                width={150}
-                height={150}
-                alt={avatar}
-                priority={true}
-              />
-            </div>
-          ))}
-        </div>
-        {loaded && instanceRef.current && (
-          <>
-            <Arrow
-              left
-              onClick={(e: any) => {
-                e.stopPropagation() || instanceRef.current?.prev();
-              }}
-              disabled={currentSlide === 0}
+    <div className="navigation-wrapper">
+      <div ref={sliderRef} className="keen-slider">
+        {Object.values(avatars).map((avatar, index) => (
+          <div key={`div-${index}`} className="keen-slider__slide number-slide">
+            <Image
+              className="rounded-full"
+              src={avatar}
+              width={150}
+              height={150}
+              alt={avatar}
+              priority={true}
             />
-
-            <Arrow
-              onClick={(e: any) => {
-                e.stopPropagation() || instanceRef.current?.next();
-              }}
-              disabled={
-                currentSlide ===
-                instanceRef.current.track.details.slides.length - 1
-              }
-            />
-          </>
-        )}
+          </div>
+        ))}
       </div>
-    </>
+      {loaded && instanceRef.current && (
+        <>
+          <Arrow
+            left
+            onClick={(e: any) => {
+              e.stopPropagation() || instanceRef.current?.prev();
+            }}
+          />
+
+          <Arrow
+            onClick={(e: any) => {
+              e.stopPropagation() || instanceRef.current?.next();
+            }}
+          />
+        </>
+      )}
+    </div>
   );
 }
 
 function Arrow(props: {
-  disabled: boolean;
   left?: boolean;
   onClick: (e: any) => void;
 }) {
-  const disabled = props.disabled ? ' arrow--disabled' : '';
   return (
     <svg
       onClick={props.onClick}
-      className={`arrow ${props.left ? 'arrow--left' : 'arrow--right'
-        } ${disabled}`}
+      className={`arrow ${props.left ? 'arrow--left' : 'arrow--right'}`}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
     >
