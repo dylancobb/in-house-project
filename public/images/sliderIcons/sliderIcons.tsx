d1aec839d2adee46dda64a56c3bb5a793e876294
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-11-06 17:14:35 +0000

    Create array Icons in sliderIcons.tsx

--- public/images/sliderIcons/sliderIcons.tsx
+++ public/images/sliderIcons/sliderIcons.tsx
@@ -4,4 +4,10 @@ const icon3 = require('./palette.svg');
 const icon4 = require('./think.svg');
 const icon5 = require('./laugh.svg');
 
-export const icons = [{ id: icon1 }];
+export const icons = [
+  { url: icon1, message: '1 Each player writes a quirky sentence' },
+  { url: icon2, message: '2 You receive each other’s sentences' },
+  { url: icon1, message: '3 Use your art skills to depict it!' },
+  { url: icon4, message: '4 Guess the sentences behind the art' },
+  { url: icon5, message: '1 Each player writes a quirky sentence' },
+];
