40add4cb82d61030df9171a5ba3b30a37ac9f79b
GeorgeKlemperer <george@klemperer.co.uk>
2023-11-10 11:38:23 +0000

    lobby refresh fixed

--- app/components/UserProfiles.tsx
+++ app/components/UserProfiles.tsx
@@ -1,48 +1,45 @@
 import React, { useEffect, useState } from "react";
 import Image from 'next/image';
 
-
-
-// Get the current URL
-const currentUrl = window.location.href;
-
-// Split the URL by "/"
-const urlParts = currentUrl.split('/');
-
-// Extract the values of game_id and username
-const gameId = parseInt(urlParts[3]);
-const username = urlParts[4];
-
-// Now you can use gameId and username in your code
-console.log('Game ID:', gameId);
-console.log('Username:', username);
-
-// UserProfiles will be displayed through a loop, this is just temporary
 const UserProfiles = () => {
-
-    const [playersArray, setPlayersArray] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-
+  const [playersArray, setPlayersArray] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [gameId, setGameId] = useState(null);
+  const [username, setUsername] = useState(null);
 
   useEffect(() => {
+    // Get the current URL
+    const currentUrl = window.location.href;
+
+    // Split the URL by "/"
+    const urlParts = currentUrl.split('/');
+
+    // Extract the values of game_id and username
+    const extractedGameId = parseInt(urlParts[3]);
+    const extractedUsername = urlParts[4];
+
+    // Now you can use gameId and username in your code
+    console.log('Game ID:', extractedGameId);
+    console.log('Username:', extractedUsername);
+
+    setGameId(extractedGameId);
+    setUsername(extractedUsername);
+
     const apiUrl = "https://4oqenpdzm6.execute-api.eu-west-2.amazonaws.com/dev/items";
-  
+
     // Fetch the data when the component is rendered
     fetch(apiUrl, {
       method: "GET",
     })
       .then((response) => response.json())
       .then((data) => {
-  
         // Find the game with the specified ID
-        const specificGame = data.find((game: any) => game.game_id === gameId);
-  
+        const specificGame = data.find((game) => game.game_id === extractedGameId);
+
         if (specificGame) {
           console.log("Found game:", specificGame);
           // Now you can handle the specific game here
           setPlayersArray(specificGame.game_stats.players);
-          // console.log("playersArray", playersArray);
         } else {
           console.log("Game not found");
         }
@@ -53,72 +50,37 @@ const UserProfiles = () => {
       .finally(() => {
         setLoading(false); // Set loading to false after fetching, regardless of success or failure
         console.log("Data fetched, loading set to false.");
-
       });
-  }, []);
+  }, []); // Empty dependency array ensures this effect runs only once, similar to componentDidMount
 
-    // Log the updated state after it has been set
-    useEffect(() => {
-      console.log("playersArray", playersArray);
-    }, [playersArray]);
-  
-    if (loading) {
-      console.log('loading')
-      return <p>Loading...</p>; // Display a loading message or spinner while fetching data
-      
-    } else {
-  
-  
-  return (
-    <>
-      <div className="flex flex-col gap-2 ">
-        {playersArray.map((player) => (
-          <div key={player.player_id} className="flex justify-around items-center p-2">
-            <Image
-              // src={`https://res.cloudinary.com/dypg1icpd/image/upload/v1699014570/samples/animals/cat.jpg`}
-              src={`/images/avatars/avatar${parseInt(player.player_avatar)+1}.jpg`}
-              width={40}
-              height={40}
-              alt="userAvatar"
-              priority={true}
-            />
-            <p>{player.player_username}</p>
-          </div>
-        ))}
-      </div>
-    </>
-    
-  );
-        }
-  
-  // return (
-  //   <>
-  //     <div className="flex flex-col gap-2 ">
-  //       <div className="flex justify-around items-center p-2">
-  //         <Image
-  //           key={1}
-  //           src={avatar1}
-  //           width={40}
-  //           height={40}
-  //           alt="userAvatar"
-  //           priority={true}
-  //         />
-  //         <p>User1</p>
-  //       </div>
-  //       <div className="flex w-28 justify-around items-center">
-  //         <Image
-  //           key={2}
-  //           src={avatar2}
-  //           width={40}
-  //           height={40}
-  //           alt="userAvatar"
-  //           priority={true}
-  //         />
-  //         <p>User2</p>
-  //       </div>
-  //     </div>
-  //   </>
-  // );
+  // Log the updated state after it has been set
+  useEffect(() => {
+    console.log("playersArray", playersArray);
+  }, [playersArray, loading]);
+
+  if (loading) {
+    console.log('loading');
+    return <p>Loading...</p>; // Display a loading message or spinner while fetching data
+  } else {
+    return (
+      <>
+        <div className="flex flex-col gap-2 ">
+          {playersArray.map((player) => (
+            <div key={player.player_id} className="flex justify-around items-center p-2">
+              <Image
+                src={`/images/avatars/avatar${parseInt(player.player_avatar) + 1}.jpg`}
+                width={40}
+                height={40}
+                alt="userAvatar"
+                priority={true}
+              />
+              <p>{player.player_username}</p>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
 };
 
 export default UserProfiles;
