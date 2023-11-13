9731fbd5aa145a972cf9b845a9b7a01bfd322aca
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-11-13 14:56:09 +0000

    Edit input component in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between py-10">
       <InstructionsSlider />
       <AvatarSlider setCurrentSlide={setCurrentSlide} />
-      <Input setStateUsername={setStateUsername}/>
+      <Input setFunction={setStateUsername} label="username" placeholder="Anonymous123"/>
       <StartButton currentSlide={currentSlide} stateUsername={stateUsername}/>
     </main>
   );
