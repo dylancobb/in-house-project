bb106edd42d1784206ec3605ff7771e0464e4a87
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-11-13 17:44:34 +0000

    console log FetchData

--- app/[game_id]/[username]/results/page.tsx
+++ app/[game_id]/[username]/results/page.tsx
@@ -1,35 +1,51 @@
 'use client'
 
-import React, {useEffect} from 'react'
-
-
+import React, { useEffect, useRef, useState } from 'react'
 
 const Results = () => {
-        useEffect(() => {
-          // Use window.location inside useEffect to ensure it's executed on the client side
-          const idUrl = window.location.href;
-          console.log("HREF " + idUrl);
-      
-          const apiUrl =
-            "https://4oqenpdzm6.execute-api.eu-west-2.amazonaws.com/dev/items?&Operation=GetMaxGameID";
-      
-          fetch(apiUrl, {
-            method: "GET",
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              if (data && data.length > 0) {
-                console.log(data);
-              } else {
-                console.error("No game data found.");
-              }
-            })
-            .catch((error) => {
-              console.error("Error fetching latest game ID:", error);
-            });
-        }, []); // The empty dependency array ensures that this useEffect runs only once, similar to componentDidMount
+  const [fetchData, setFetchData] = useState(null)
+
+  useEffect(() => {
+    // Use window.location inside useEffect to ensure it's executed on the client side
+    const currentUrl = window.location.href
+
+    // Split the URL by "/"
+    const urlParts = currentUrl.split('/')
+    // Extract the values of game_id and username
+    const extractedGameId = parseInt(urlParts[3])
+    console.log(
+      'Make sure it s the right Id number from url: ' + extractedGameId
+    )
+
+    const apiUrl = `https://4oqenpdzm6.execute-api.eu-west-2.amazonaws.com/dev/items/${extractedGameId}`
+
+    fetch(apiUrl, {
+      method: 'GET',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          setFetchData(data)
+          console.log(data)
+          console.log(fetchData)
+        } else {
+          console.error('No game data found.')
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching latest game ID:', error)
+      })
+  }, [])
+
+  useEffect(() => {
+    console.log('Updated fetchData:', fetchData)
+  }, [fetchData]) // The empty dependency array ensures that this useEffect runs only once, similar to componentDidMount
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-10">results</main>
+    <main className='flex min-h-screen flex-col items-center justify-between py-10'>
+      <div></div>
+      results
+    </main>
   )
 }
 
