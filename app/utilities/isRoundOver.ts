6bd11dbc4526360f3a289695cb275824d163ed63
GeorgeKlemperer <george@klemperer.co.uk>
2023-11-13 17:12:49 +0000

    Working but needs refresh

---
+++ app/utilities/isRoundOver.ts
@@ -0,0 +1,27 @@
+export default async function isRoundOverFunction(gameId, round) {
+    try {
+        let apiUrl = `https://4oqenpdzm6.execute-api.eu-west-2.amazonaws.com/dev/items/${gameId}`;
+
+        const response = await fetch(apiUrl, {
+            method: "GET",
+        });
+
+        const data = await response.json();
+
+        // Access the 'game_stats' property
+        const gameStats = data.game_stats;
+
+        // Access the 'players' property of 'game_stats'
+        const players = gameStats.players;
+
+        // Check if every player has submitted a prompt
+        const isRoundOver = players.every(player => player.player_prompt.trim() !== '');
+
+        return isRoundOver;
+    } catch (error) {
+        // Handle errors here
+        console.error("Error fetching data:", error);
+        // Return false in case of an error
+        return false;
+    }
+}
