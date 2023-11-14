a82f8cf479ef08a376398464cef6db293250a9a3
Dylan <122552492+dylancobb@users.noreply.github.com>
2023-11-14 15:07:09 +0000

    duplicate imports in prompt page

--- app/[game_id]/[username]/prompt/page.tsx
+++ app/[game_id]/[username]/prompt/page.tsx
@@ -1,12 +1,5 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
-import Image from 'next/image'
-import Input from '../../../components/Input'
-import SubmitButton from '../../../components/Button/SubmitButton'
-import turnTakenFunction from '@/app/utilities/turnTakenFunction'
-import isRoundOverFunction from '@/app/utilities/isRoundOver'
-import notepad from '@/public/images/sliderIcons/pen.svg'
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Input from "../../../components/Input";
