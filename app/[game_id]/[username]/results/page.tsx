94c82590d0ddcf742caac8f100544c7c3563b282
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-11-14 16:05:29 +0000

    displaying right pics?

--- app/[game_id]/[username]/results/page.tsx
+++ app/[game_id]/[username]/results/page.tsx
@@ -17,9 +17,6 @@ const Results: React.FC = () => {
     const currentUrl = window.location.href
     const urlParts = currentUrl.split('/')
     const extractedGameId = parseInt(urlParts[3])
-    console.log(
-      'Make sure it s the right Id number from url: ' + extractedGameId
-    )
 
     const apiUrl = `https://4oqenpdzm6.execute-api.eu-west-2.amazonaws.com/dev/items/${extractedGameId}`
 
@@ -39,26 +36,106 @@ const Results: React.FC = () => {
       })
   }, [])
 
+  //   return (
+  //     <main className='flex min-h-screen flex-col items-center justify-between py-10'>
+  //       <div>
+  //         {players ? (
+  //           players.map((player, index) => {
+  //             if (index !== players.length - 1) {
+  //               return (
+  //                 <div key={player.player_id}>
+  //                   <div>
+  //                     <p>Username: {player.player_username}</p>
+  //                     <p>Avatar: {player.player_avatar}</p>
+  //                     <p>Prompt: {player.player_prompt}</p>
+  //                   </div>
+  //                   <Image
+  //                     className='bg-white'
+  //                     src={players[index + 1].player_drawing}
+  //                     width={200}
+  //                     height={200}
+  //                     alt="player's painting"
+  //                     priority={true}
+  //                   />
+  //                   <p>Guess: {players[index + 2].player_guess}</p>
+  //                 </div>
+  //               )
+  //             } else if (index === players.length - 2) {
+  //               {
+  //                 return (
+  //                   <div key={player.player_id}>
+  //                     <div>
+  //                       <p>Username: {player.player_username}</p>
+  //                       <p>Avatar: {player.player_avatar}</p>
+  //                       <p>Prompt: {player.player_prompt}</p>
+  //                     </div>
+  //                     <Image
+  //                       className='bg-white'
+  //                       src={players[2].player_drawing}
+  //                       width={200}
+  //                       height={200}
+  //                       alt="player's painting"
+  //                       priority={true}
+  //                     />
+  //                     <p>Guess: {players[0].player_guess}</p>
+  //                   </div>
+  //                 )
+  //               }
+  //             } else {
+  //               return (
+  //                 <div key={player.player_id}>
+  //                   <div>
+  //                     <p>Username: {player.player_username}</p>
+  //                     <p>Avatar: {player.player_avatar}</p>
+  //                     <p>Prompt: {player.player_prompt}</p>
+  //                   </div>
+  //                   <Image
+  //                     className='bg-white'
+  //                     src={players[0].player_drawing}
+  //                     width={200}
+  //                     height={200}
+  //                     alt="player's painting"
+  //                     priority={true}
+  //                   />
+  //                   <p>Guess: {players[1].player_guess}</p>
+  //                 </div>
+  //               )
+  //             }
+  //           })
+  //         ) : (
+  //           <p>Loading...</p>
+  //         )}
+  //       </div>
+  //       results
+  //     </main>
+  //   )
+  // }
   return (
     <main className='flex min-h-screen flex-col items-center justify-between py-10'>
       <div>
         {players ? (
-          players.map((player) => (
-            <div key={player.player_id}>
-              <div>
-                <p>Username: {player.player_username}</p>
-                <p>Avatar: {player.player_avatar}</p>
+          players.map((player, index) => {
+            const nextIndex = (index + 1) % players.length
+            const nextNextIndex = (index + 2) % players.length
+
+            return (
+              <div key={player.player_id}>
+                <div>
+                  <p>Username: {player.player_username}</p>
+                  <p>Prompt: {player.player_prompt}</p>
+                </div>
+                <Image
+                  className='bg-white'
+                  src={players[nextIndex].player_drawing}
+                  width={200}
+                  height={200}
+                  alt="player's painting"
+                  priority={true}
+                />
+                <p>Guess: {players[nextNextIndex].player_guess}</p>
               </div>
-              <Image
-                className='bg-white'
-                src={player.player_drawing}
-                width={200}
-                height={200}
-                alt="player's painting"
-                priority={true}
-              />
-            </div>
-          ))
+            )
+          })
         ) : (
           <p>Loading...</p>
         )}
@@ -67,5 +144,4 @@ const Results: React.FC = () => {
     </main>
   )
 }
-
 export default Results
