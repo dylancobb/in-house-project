ab8338612c05e788b889a311719cda9047abaf2d
Dylan <122552492+dylancobb@users.noreply.github.com>
2023-11-14 15:04:34 +0000

    manually merging in code from DevBranch

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import HomeStartButton from "./components/Button/HomeStartButton";
+import StartButton from "./components/Button/StartButton";
 import AvatarSlider from "./components/AvatarSlider/AvatarSlider";
 import InstructionsSlider from "./components/InstructionsSlider/InstructionsSlider";
 import Input from "./components/Input";
@@ -14,8 +14,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between py-10">
       <InstructionsSlider />
       <AvatarSlider setCurrentSlide={setCurrentSlide} />
-      <Input setStateUsername={setStateUsername}/>
-      <HomeStartButton currentSlide={currentSlide} stateUsername={stateUsername} />
+      <Input setFunction={setStateUsername} label="username" placeholder="Anonymous123"/>
+      <StartButton currentSlide={currentSlide} stateUsername={stateUsername}/>
     </main>
   );
 }
